6635d6b4953df3bc63355815e2c9d6f6eea73b40
Neha Anand <neha280103@gmail.com>
2022-06-04 22:21:35 +0530

    changed img links

--- src/components/Section.js
+++ src/components/Section.js
@@ -36,7 +36,7 @@ function Section({ title, description, leftBtnTxt, rightBtnTxt,backgroundImg }) 
             <RightButton>{rightBtnTxt}</RightButton>
           )}
         </ButtonGroup>
-        <DownArrow src="./images/down-arrow.svg" alt="down-arrow" />
+        <DownArrow src="images/down-arrow.svg" alt="down-arrow" />
       </Buttons>
     </Wrap>
   );
@@ -51,12 +51,12 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
+    background-image: url('images/model-s.jpg');
     display:flex;
     flex-direction: column;
     justify-content:space-between ;//vertical alignment
     align-items:center;//horizontal alignment
-    background-image:${props => `url("/images/${props.bgImage}")`} ;
+    background-image:${props => `url("images/${props.bgImage}")`} ;
 `
 
 const ItemText = styled.div`
