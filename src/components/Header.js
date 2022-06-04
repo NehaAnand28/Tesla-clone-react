4f3814899ce2f86e0a79f973e1cb5dec84cf4d72
Neha Anand <neha280103@gmail.com>
2022-06-04 20:33:01 +0530

    data change in sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,7 @@ function Header() {
 
   const [burgerOpen,setBurgerOpen] = useState(false);
   const cars = useSelector(selectCars)
-  // console.log(cars);
-
-  // function setBurgerOpen(){
-    
-  // }
+ 
 
   return (
     <Container>
@@ -38,10 +34,18 @@ function Header() {
           <li><a href="#">Existing Inventory</a></li>
           <li><a href="#">Used Inventory</a></li>
           <li><a href="#">Trade-in</a></li>
+          <li><a href="#">Test Drive</a></li>
+          <li><a href="#">Insurance</a></li>
           <li><a href="#">Cybertruck</a></li>
           <li><a href="#">Roadaster</a></li>
           <li><a href="#">Semi</a></li>
-          <li><a href="#">Batteries</a></li>
+          <li><a href="#">Charging</a></li>
+          <li><a href="#">Powerwall</a></li>
+          <li><a href="#">Commercial Energy</a></li>
+          <li><a href="#">Utilities</a></li>
+          <li><a href="#">Find Us</a></li>
+          <li><a href="#">Support</a></li>
+          <li><a href="#">Investor Relations</a></li>
         </BurgerNav>
 
     </Container>
