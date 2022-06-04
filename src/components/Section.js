0374fe9e167e9386c8257e7cc93a8b094ca9a1e1
Neha Anand <neha280103@gmail.com>
2022-06-04 17:05:24 +0530

    added different sections using props

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section({ title, description, leftBtnTxt, rightBtnTxt,backgroundImg }) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnTxt}</LeftButton>
+          <RightButton>{rightBtnTxt}</RightButton>
         </ButtonGroup>
         <DownArrow src="./images/down-arrow.svg" alt="down-arrow" />
       </Buttons>
@@ -33,6 +33,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content:space-between ;//vertical alignment
     align-items:center;//horizontal alignment
+    background-image:${props => `url("/images/${props.bgImage}")`} ;
 `
 
 const ItemText = styled.div`
@@ -43,6 +44,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display:flex;
     margin-bottom:30px;
+    @media(max-width:768px){
+        flex-direction:column ;
+    }
 `
 
 const LeftButton = styled.div`
